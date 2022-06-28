3ed9ffbae079dd7b5f5afd2f19c70e7844bed5c7
gvarys <johnnyzaev@gmail.com>
2022-06-28 14:43:35 +0300

    Main menu added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void LoadLevel(int level)
+	{
+		PlayerPrefs.SetInt("level", level);
+		SceneManager.LoadScene(0);
+	}
+}
